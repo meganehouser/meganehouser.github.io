c19e7f0d6dd0755cd3c0b2833ca6986e1b35dc35
meganehouser <sleepy.st818@gmail.com>
2016-02-01 00:05:00 +0900

    add files.

---
+++ content/photo/2015-06-24_hydrangea.md
@@ -0,0 +1,28 @@
+Title: 紫陽花とファインダー
+Tags: E-PL2
+Summary: 鎌倉へ紫陽花を見に行った。
+
+<a href="https://www.flickr.com/photos/125573348@N07/18858969771" title="Untitled by megane houser, on Flickr">
+<img src="https://c2.staticflickr.com/6/5601/18858969771_5a903030ea_z.jpg" width="640" height="480" alt="Untitled"></a>
+
+<a href="https://www.flickr.com/photos/125573348@N07/18670429259" title="Untitled by megane houser, on Flickr">
+<img src="https://c2.staticflickr.com/6/5574/18670429259_e94551f77d_z.jpg" width="640" height="480" alt="Untitled"></a>
+
+<a href="https://www.flickr.com/photos/125573348@N07/18856059785" title="Untitled by megane houser, on Flickr">
+<img src="https://c2.staticflickr.com/6/5535/18856059785_f763fb02a1_z.jpg" width="640" height="480" alt="Untitled"></a>
+
+<a href="https://www.flickr.com/photos/125573348@N07/18852065732" title="Untitled by megane houser, on Flickr">
+<img src="https://c2.staticflickr.com/6/5463/18852065732_d9203e3940_z.jpg" width="640" height="480" alt="Untitled"></a>
+
+<a href="https://www.flickr.com/photos/125573348@N07/18857457185" title="Untitled by megane houser, on Flickr">
+<img src="https://c2.staticflickr.com/6/5482/18857457185_596e8fa110_z.jpg" width="640" height="480" alt="Untitled"></a>
+
+写真を撮るのは楽しい。でも、懐古趣味みたいで嫌なのだが、学生時代にアナログカメラを持ち歩いていた時の方が楽しめていた感じがする。
+
+銀塩からデジタルになり、色や質感が変わったこともあるのだが、個人的にはファインダーが大きな要素だったのではと思っている。
+
+ファインダーに目を押し付け、暗がりから世界を覗き込む感じが好きだった。
+ファインダーを通した世界は一種の異化作用が働き、僕は少しでも新鮮な感動を定着させようと、四角の枠で切り取られた光景に集中することができた。
+
+ライブビューが全盛となり、ファインダーがなかったり、オプションになってしまったのは、残念なのだった。
+外付けのファインダー買うかな。
---
+++ entry.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python
+import os
+from datetime import datetime
+from argparse import ArgumentParser
+from pathlib import Path
+import requests
+import json
+
+
+FLICKR_URL = 'https://api.flickr.com/services/rest/'
+
+
+def input_str(name, default):
+    print('{0} [{1}]: '.format(name, default))
+    value = input('> ')
+    if value:
+        return value
+    else:
+        return default
+
+
+def make_photos(api_key, album, userid, tag):
+    jd = json.JSONDecoder()
+
+    def get_json(text):
+        json_txt = r.text.replace('jsonFlickrApi(', '').replace(')', '')
+        print(json_txt)
+        return jd.decode(json_txt)
+
+    query = {'method': 'flickr.photosets.getPhotos',
+             'api_key': api_key,
+             'user_id': userid,
+             'photoset_id': album,
+             'format': 'json',
+             'extras': 'tags'}
+    r = requests.post(FLICKR_URL, query)
+    j = get_json(r.text)
+    photos = [p['id'] for p in j['photoset']['photo'] if tag in p['tags']]
+
+    photo_links = []
+    link_query = {'method': 'flickr.photos.getSizes',
+                  'api_key': api_key, 'format': 'json'}
+    for pid in photos:
+        link_query['photo_id'] = pid
+        r = requests.post(FLICKR_URL, link_query)
+        j = get_json(r.text)
+        print(j)
+        photo_links.append(j['sizes']['size'][5])
+    return photo_links
+
+
+def make_entry(content_dir, category, tags, date, suffix, content):
+    cate_dir = os.path.join(content_dir, category)
+    p = Path(cate_dir)
+    if not p.exists():
+        p.mkdir()
+
+    filename = '{0}_{1}.md'.format(date, suffix)
+    filepath = os.path.join(cate_dir, filename)
+    with open(filepath, 'w') as f:
+        f.write("Title: " + os.linesep)
+        f.write("Tags: " + ', '.join(tags) + os.linesep)
+        f.write("Summary: " + os.linesep)
+        if content:
+            f.write(os.linesep + content)
+
+        return filepath
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument("-c", "--category", help="define category")
+    parser.add_argument("-d", "--date", help="define date")
+    parser.add_argument("-s", "--suffix", help="add file name suffix")
+    parser.add_argument("-e", "--edit", help="edit the blog entry")
+    parser.add_argument("-p", "--photo", action='store_true',
+                        help="make a photo link entry")
+    parser.add_argument("-t", "--tag", help="photo tag")
+    args = parser.parse_args()
+
+    category = ""
+    tags = []
+    date = datetime.now()
+    suffix = ""
+
+    if args.category:
+        category = args.category
+    else:
+        category = input_str("category", category)
+
+    if args.date:
+        date = datetime.strptime(args.date, '%Y-%m-%d')
+    else:
+        date = input_str("date", '{0:%Y-%m-%d}'.format(date))
+
+    if args.suffix:
+        suffix = args.suffix
+    else:
+        suffix = input_str("suffix", suffix)
+
+    content = ''
+    if args.photo:
+        api_key = os.environ['Flickr_APIKEY']
+        album = os.environ['Flickr_ALBUM']
+        userid = os.environ['Flickr_USER']
+        photos = make_photos(api_key, album, userid, args.tag)
+        link_fmt = "<a href='{0}' width=640 height=480 alt='untitled'>{1}" \
+                   "</a>{2}"
+        for p in photos:
+            content += link_fmt.format(p['url'], p['source'], os.linesep)
+
+    content_dir = './content'
+    if 'blog_dir' in os.environ:
+        content_dir = os.environ['blog_dir']
+
+    filepath = make_entry(content_dir, category, tags, date, suffix, content)
+
+    if not args.edit:
+        print('create {0}'.format(filepath))
+    else:
+        editor = 'vim'
+        if 'blog_editor' in os.environ:
+            editor = os.environ['blog_editor']
+
+        os.system(editor + ' ' + filepath)
